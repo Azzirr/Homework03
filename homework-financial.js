331f53939ea245f280e250b528ab136d4b445416
Arkadiusz Sliwowski <arek.sliwowski37@gmail.com>
2022-11-12 19:34:18 +0100

    Zadanie A 1-3; Zadanie B ruszone

--- homework-financial.js
+++ homework-financial.js
@@ -8,13 +8,76 @@ function getFiancialObject() {
   function moneySpent(){
     const dateJan = new Date('January 01, 2014 00:00:00');
     const dateDec = new Date('December 31, 2014 23:59:59');
-    const timeJan = dateJan.getTime();
+    const timeJan = dateJan.getTime(); // time in ms
     const timeDec = dateDec.getTime();
-    let dupa = financialObject[1].detailsOfPayent.date
-    
-
+    let sumUpCosts = 0;
+    for(let i = 0; i < financialData.length; i++){
+      var date = financialData[i].detailsOfPayent.date;
+      var datearray = date.split("-");
+      var newdate = datearray[1] + '-' + datearray[0] + '-' + datearray[2];
+      let dateInMiliseconds = Date.parse(newdate)
+      if(dateInMiliseconds < timeDec && dateInMiliseconds > timeJan){
+        sumUpCosts = sumUpCosts + parseFloat(financialData[i].cost)
+      }
+    }
+    console.log('Costs in 2014 = ' + sumUpCosts.toFixed(2)) // 46794.860000000015 XD
   }
-  moneySpent()
+  // moneySpent()
+
+  function earningsPerCompany(){
+    let earningsECSTASIA = 0;
+    let earningsMANGELICA = 0;
+    let earningsCODAX = 0;
+    let result = financialData.reduce((count, currIndex) => {
+      if(currIndex.detailsOfPayent.company === "ECSTASIA"){
+        return earningsECSTASIA += parseFloat(currIndex.cost)
+      } else if(currIndex.detailsOfPayent.company === "MANGELICA"){
+        return earningsMANGELICA += parseFloat(currIndex.cost)
+      } else if(currIndex.detailsOfPayent.company === "CODAX"){
+        return earningsCODAX += parseFloat(currIndex.cost)
+      } 
+    }, 0)
+    console.log('ECSTASIA costs ' + earningsECSTASIA);
+    console.log('MANGELICA costs ' + earningsMANGELICA);
+    console.log('CODAX costs ' + earningsCODAX);
+  }
+  // earningsPerCompany()
+
+  function spendingsPerTransactionType(){
+    let type1 = 0;
+    let type2 = 0;
+    let type3 = 0;
+    let type4 = 0;
+    let type5 = 0;
+    const result = financialData.filter((currentIndex) => {
+      if(currentIndex.detailsOfPayent.Type === 1){
+        return type1 += parseFloat(currentIndex.cost)
+      } else if(currentIndex.detailsOfPayent.Type === 2){
+        return type2 += parseFloat(currentIndex.cost)
+      } else if(currentIndex.detailsOfPayent.Type === 3){
+        return type3 += parseFloat(currentIndex.cost)
+      } else if(currentIndex.detailsOfPayent.Type === 4){
+        return type4 += parseFloat(currentIndex.cost)
+      } else if(currentIndex.detailsOfPayent.Type === 5){
+        return type5 += parseFloat(currentIndex.cost)
+      }
+    })
+    console.log('Type 1 costs ' + type1.toFixed(2))
+    console.log('Type 2 costs ' + type2.toFixed(2))
+    console.log('Type 3 costs ' + type3.toFixed(2))
+    console.log('Type 4 costs ' + type4.toFixed(2))
+    console.log('Type 5 costs ' + type5.toFixed(2))
+  }
+  // spendingsPerTransactionType()
+  // TO DO!!!
+  // function spendingsByMonth(){
+  //   // let fullDate = currentIndex.detailsOfPayent.date;
+  //   // let monthDate = fullDate.slice(3, 5);
+  //   const result = financialData.reduce((accumulator, element) => {
+
+  //   }, 0)
+  // }
+  // spendingsByMonth()
  // return financialObject;
 }
 getFiancialObject()
--- homework-starwars.js
+++ homework-starwars.js
@@ -9,18 +9,29 @@ console.log(
     sumAllStarshipsCostFromEpisodes(4, 6)
 );
 
-function sumAllStarshipsCostFromEpisodes(startEp, endEp) {
-  let sum = 0;
-  // TODO
-  return sum;
-}
+// function sumAllStarshipsCostFromEpisodes(startEp, endEp) {
+//   let sum = 0;
+//   //keys for all episodes
+//   let episode = [];
+//   for(let i = startEp; i <= endEp; i++){
+//     episode.push(i)
+//   }
+//   for(let i = 0; i < episode.length; i++){
+//       const result = starships.forEach((element) => {
+//         if(episode[i] === films.episode_id){
+//           sum += 
+//         }
+//       })
+//   }
 
+//   return sum;
+// }
 // find the fastest starship you can afford having 8500000 credits
 
-console.log(
-  'Fastest ship I can get for up to 8500000 is: ' +
-    getFastestShipFor(8500000).name
-);
+// console.log(
+//   'Fastest ship I can get for up to 8500000 is: ' +
+//     getFastestShipFor(8500000).name
+// );
 
 function getFastestShipFor(money) {
   let ship;
@@ -30,10 +41,10 @@ function getFastestShipFor(money) {
 
 // find planet name with the lowest difference between the rotation period and orbital period
 
-console.log(
-  'Planet name with the lowest difference between the rotation period and orbital period is: ' +
-    getPlanetNameWithLowestDifference('rotation_period', 'orbital_period')
-);
+// console.log(
+//   'Planet name with the lowest difference between the rotation period and orbital period is: ' +
+//     getPlanetNameWithLowestDifference('rotation_period', 'orbital_period')
+// );
 
 function getPlanetNameWithLowestDifference(key1, key2) {
   let planetName;
@@ -43,10 +54,10 @@ function getPlanetNameWithLowestDifference(key1, key2) {
 
 // map all starships with crew <= 4 that were created between 10 dec 2014 and 15 dec 2014
 
-console.log(
-  'Ships with max crew of 4 created between 10.12.2014 - 12.12.2014 number is: ' +
-    getCrewShipFrom(4, new Date(2014, 12, 10), new Date(2014, 12, 12)).length
-);
+// console.log(
+//   'Ships with max crew of 4 created between 10.12.2014 - 12.12.2014 number is: ' +
+//     getCrewShipFrom(4, new Date(2014, 12, 10), new Date(2014, 12, 12)).length
+// );
 
 function getCrewShipFrom(maxCrew, dateStart, dateEnd) {
   let ship;
@@ -56,10 +67,10 @@ function getCrewShipFrom(maxCrew, dateStart, dateEnd) {
 
 // create an array of people’s names from episodes 1 and 5 sorted by the diameter of origin planet low to high
 
-console.log(
-  'People from ep 1 - 5 sorted by origin planet diameter low to high: ' +
-    getPeopleSortedByOriginPlanetDiameter(1, 5)
-);
+// console.log(
+//   'People from ep 1 - 5 sorted by origin planet diameter low to high: ' +
+//     getPeopleSortedByOriginPlanetDiameter(1, 5)
+// );
 
 function getPeopleSortedByOriginPlanetDiameter(startEp, endEp) {
   let people;
